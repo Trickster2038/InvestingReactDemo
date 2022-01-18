7c509c6de0de4418934958436e999f78dfd27dc8
Sergey <33185046+Trickster2038@users.noreply.github.com>
2022-01-18 16:38:36 +0300

    editing view

---
+++ front/my-app/src/components/List.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class List extends React.Component {
+    render() {
+      return (
+        <ul>
+            <li>Point A</li>
+            <li>Point B</li>
+            <li>Point C</li>
+        </ul>
+      );
+    }
+  }
+
+  export default List;
